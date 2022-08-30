87e9912e80d194acd3d17d7988058bc38e5b4d72
Simon Wacziarg <swacziarg@gmail.com>
2022-08-30 12:04:49 -0700

    added methods with contents that should work (untested)

--- src/atmain.java
+++ src/atmain.java
@@ -1,24 +1,39 @@
+import java.util.HashMap;
 
 public class atmain {
+	HashMap<Integer, Double> accs;
+	public atmain() {
+		accs = new HashMap<Integer, Double>();
+	}
 	public void openAccount(int accountNum){
 		
+		accs.put(accountNum, 0.0);
 	}
 	public void openAccount(int accountNum, double deposit) {
+		accs.put(accountNum, deposit);
 		
 	}
 	public void closeAccount(int accountNum){
-		
+		accs.remove(accountNum);
 	}
 	public double checkBalance(int accountNum) {
-		
+		if (accs.get(accountNum) != null) {
+			return accs.get(accountNum);
+		}
 		return 0.0;
 	}
 	public boolean depositMoney(int accountNum, double deposit) {
-		
+		if (accs.get(accountNum) != null) {
+			accs.put(accountNum, (accs.get(accountNum) + deposit));
+			return true;
+		}
 		return false;
 	}
 	public boolean withdrawMoney(int accountNum, double wAmt) {
-		
+		if (accs.get(accountNum) != null) {
+			accs.put(accountNum, accs.get(accountNum) - wAmt);
+			return true;
+		}
 		return false;
 	}
 }
