1e9297005d9ea2b091e0e279ad4d6b79b96a1d33
Simon Wacziarg <swacziarg@gmail.com>
2022-08-30 11:52:53 -0700

    added methods

--- src/atmain.java
+++ src/atmain.java
@@ -1,4 +1,24 @@
 
 public class atmain {
-	
+	public void openAccount(int accountNum){
+		
+	}
+	public void openAccount(int accountNum, double deposit) {
+		
+	}
+	public void closeAccount(int accountNum){
+		
+	}
+	public double checkBalance(int accountNum) {
+		
+		return 0.0;
+	}
+	public boolean depositMoney(int accountNum, double deposit) {
+		
+		return false;
+	}
+	public boolean withdrawMoney(int accountNum, double wAmt) {
+		
+		return false;
+	}
 }
